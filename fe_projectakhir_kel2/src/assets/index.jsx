4472c1ee11348ac8a91199a8c5e460f1e1ba65c4
Fitrah Pratama, Muhammad <fitrakadev@gmail.com>
2020-11-30 00:34:02 +0700

    + Style, Header, & Font
    Added styles theme colors, header component, and google's font family: 'Fredoka One' into project

---
+++ fe_projectakhir_kel2/src/assets/index.jsx
@@ -0,0 +1,2 @@
+export {default as logo}    from './logo.png'
+export {default as d_user}  from './d_user.png'
---
+++ fe_projectakhir_kel2/src/components/header/header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './header.css'
+import { logo, d_user } from '../../assets';
+
+export default () => {
+    const [profilePic,setProfilePic]=('');
+
+    return (
+        <header>
+            <div className="logoBx">
+                <div className="imgBx">
+                    <img src={logo}/>
+                </div>
+                <h8 className="slogan">
+                    <span>care</span> know no <span>distance</span>
+                </h8>
+            </div>
+            <div className="userBx">
+                <div className="buttonBx">
+                    <div className="buttonBorder" />
+                    <button>Sign in</button>
+                </div>
+                <div className="imgBx">
+                    {
+                        profilePic?
+                        <img src={profilePic} />
+                        :
+                        <img src={d_user} />
+                    }
+                </div>
+            </div>
+        </header>
+    );
+};
