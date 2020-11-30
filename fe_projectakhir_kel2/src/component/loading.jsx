6b3eb3c016d874fb25878e0eb983286febc6438d
Wili0710 <66407599+wili0710@users.noreply.github.com>
2020-11-30 15:08:34 +0700

    FE Register 20%
    Baru tampilan. Belum 100% terhubung sama backend.

---
+++ fe_projectakhir_kel2/src/component/loading.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { css } from "@emotion/core";
+import { DotLoader, PulseLoader } from "react-spinners";
+
+const override = css`
+display: block;
+margin: 0 auto;
+border-color: red;
+`;
+
+export const FullPageLoading=(loading,size,color)=>{
+    return (
+        <div className="sweet-loading">
+        <DotLoader
+            css={override}
+            size={size}
+            color={color}
+            loading={loading}
+        />
+      </div>
+    )
+}
+
+export const ButtonLoading=(loading,size,color)=>{
+    return (
+        <div className="sweet-loading">
+        <PulseLoader
+            css={override}
+            size={size}
+            color={color}
+            loading={loading}
+        />
+      </div>
+    )
+}
+
+
