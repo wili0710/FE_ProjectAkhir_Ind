5cea314fa2783c559cc9ab452a6b57d67a1f7598
Wili0710 <66407599+wili0710@users.noreply.github.com>
2020-12-02 17:45:41 +0700

    upd

--- fe_projectakhir_kel2/src/pages/admin/adminpayment.jsx
+++ fe_projectakhir_kel2/src/pages/admin/adminpayment.jsx
@@ -13,6 +13,9 @@ import { API_URL_SQL } from '../../helpers/apiUrl';
 import moment from 'moment';
 import numeral from 'numeral'
 import ReactImageMagnify from 'react-image-magnify';
+import HeaderAdmin from '../../components/header/headerAdmin';
+import {FaUserCog,FaMoneyCheckAlt} from 'react-icons/fa'
+import './user.css'
 
 const useStyles = makeStyles({
   root: {
@@ -40,9 +43,8 @@ const AdminPayment=()=>{
     })
   },[])
 
-  const onConfirmClick=(data)=>{
-      console.log(data)
-    Axios.post(`${API_URL_SQL}/payment/confirmpayment`,{id:data})
+  const onConfirmClick=(payment_id,transaksi_id)=>{
+    Axios.post(`${API_URL_SQL}/payment/confirmpayment`,{payment_id:payment_id,transaksi_id:transaksi_id})
     .then((res)=>{
         console.log(res)
         setPaymentInWaiting(res.data)
@@ -80,7 +82,7 @@ const AdminPayment=()=>{
             <TableCell style={{width:170}} align="center">{moment(val.tglexp).format('Do MMMM YYYY')}</TableCell>
             <TableCell style={{width:160}} align="center">{numeral(val.totaltransaksi).format('0,0.0')}</TableCell>
             <TableCell style={{width:110}} align="center">
-              <button onClick={()=>onConfirmClick(val.payment_id)}>Confirm</button>
+              <button onClick={()=>onConfirmClick(val.payment_id,val.transaksi_id)}>Confirm</button>
             </TableCell>
         </TableRow>
       )
@@ -88,27 +90,45 @@ const AdminPayment=()=>{
   }
 
   return (
+    <>
+      <div className="user-container">
+      <HeaderAdmin/>
+          <div className="user-right">
+              <div className="header-user">
+                  <div className="icon-group">
+                      <FaMoneyCheckAlt className="icon-size" color="black"/>
+                      <p style={{fontWeight:'600'}}>Payment</p>
+                  </div>
+              </div>
+              <div className="container-data">
 
-    <Paper className={classes.root}>
-        <TableContainer className={classes.container}>
-            <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-                <TableRow>
-                    <TableCell align="center">Payment Id</TableCell>
-                    <TableCell align="center">Transaksi Id</TableCell>
-                    <TableCell align="center">Bukti Transfer</TableCell>
-                    <TableCell align="center">Tgl Transaksi</TableCell>
-                    <TableCell align="center">Tgl Expired</TableCell>
-                    <TableCell align="center">Total Harus Dibayar</TableCell>
-                    <TableCell align="center"></TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {renderTable()}
-            </TableBody>
-            </Table>
-        </TableContainer>
-    </Paper>
+              
+                {/* <Paper className={classes.root}> */}
+                    <TableContainer>
+                        <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">Payment Id</TableCell>
+                                <TableCell align="center">Transaksi Id</TableCell>
+                                <TableCell align="center">Bukti Transfer</TableCell>
+                                <TableCell align="center">Tgl Transaksi</TableCell>
+                                <TableCell align="center">Tgl Expired</TableCell>
+                                <TableCell align="center">Total Harus Dibayar</TableCell>
+                                <TableCell align="center"></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {renderTable()}
+                        </TableBody>
+                        </Table>
+                    </TableContainer>
+                {/* </Paper> */}
+              </div>
+          </div>
+      </div>
+      </>
+    
+
   );
 }
  
