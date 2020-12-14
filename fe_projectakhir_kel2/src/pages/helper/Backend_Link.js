c733669ce840ca96d137243ef60c587096e23cc9
Wili0710 <66407599+wili0710@users.noreply.github.com>
2020-12-15 02:38:09 +0700

    back

--- fe_projectakhir_kel2/src/pages/helper/Backend_Link.js
+++ fe_projectakhir_kel2/src/pages/helper/Backend_Link.js
@@ -1,4 +1,6 @@
-export const Backend_Link = 'https://hearttoheart-kel2.herokuapp.com' `http://h2h.wiliromario.com`
+// export const Backend_Link = 'https://hearttoheart-kel2.herokuapp.com' 
+export const Backend_Link = 'http://localhost:8000' 
+// `http://h2h.wiliromario.com`
 
 // export const API_URL=`http://localhost:4000`
 // export const API_URL_SQL = `http://localhost:8000`
--- fe_projectakhir_kel2/src/pages/helper/homeUrl.jsx
+++ fe_projectakhir_kel2/src/pages/helper/homeUrl.jsx
@@ -1,4 +1,5 @@
-export const HOME_URL=`http://h2h.wiliromario.com`
+// export const HOME_URL=`http://h2h.wiliromario.com`
+export const HOME_URL=`http://localhost:3000`
 
 // export const API_URL=`http://localhost:4000`
 // export const API_URL_SQL = `http://localhost:8000`
