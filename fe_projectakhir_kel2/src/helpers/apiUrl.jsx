9705758f42d329e86e9fcf67c72ee4342ab5ba23
Wili0710 <66407599+wili0710@users.noreply.github.com>
2020-12-15 15:17:03 +0700

    change apiurl ke local

--- fe_projectakhir_kel2/src/helpers/apiUrl.jsx
+++ fe_projectakhir_kel2/src/helpers/apiUrl.jsx
@@ -1,9 +1,9 @@
-// export const API_URL=`http://localhost:4000`
-// export const API_URL_SQL = `http://localhost:8000`
-// export const HOME_URL=`http://localhost:3000`
 export const API_URL=`http://localhost:4000`
-export const API_URL_SQL = 'https://hearttoheart-kel2.herokuapp.com'
-export const HOME_URL=`http://h2h.wiliromario.com`
+export const API_URL_SQL = `http://localhost:8000`
+export const HOME_URL=`http://localhost:3000`
+// export const API_URL=`http://localhost:4000`
+// export const API_URL_SQL = 'https://hearttoheart-kel2.herokuapp.com'
+// export const HOME_URL=`http://h2h.wiliromario.com`
 
  
 export const priceFormatter = (num) => {
